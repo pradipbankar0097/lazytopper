405103435f2fe09c963f9354ea16935d563025b2
pradipbankar0097 <pradipbankar0097@gmail.com>
2021-05-18 21:11:51 +0530

    commit

--- lazytopper/settings.py
+++ lazytopper/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+
+#manually added
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , "static"),
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
