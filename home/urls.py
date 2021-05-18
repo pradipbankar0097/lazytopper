7f9a1c54dc344a6202f339a14b33f2acaea8a945
pradipbankar0097 <pradipbankar0097@gmail.com>
2021-05-18 20:43:17 +0530

    commit

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('search/', views.search, name='search')
     
